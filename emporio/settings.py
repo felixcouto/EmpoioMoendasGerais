08b586996c88ff4da65e14d9f536dfb598ef146a
Felipe <felipe@MacBook-Pro.local>
2020-06-11 09:50:41 -0300

    Create and set  template/home_page.html urls.py settings.py and views.py

--- emporio/settings.py
+++ emporio/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'emporio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
