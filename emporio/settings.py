7711ef415fe91eaef10f515e914ee73081d3b9a8
Felipe <felipe@MacBook-Pro.local>
2020-06-11 22:13:05 -0300

    Create Login and Register Forms

--- emporio/settings.py
+++ emporio/settings.py
@@ -78,6 +78,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'emporio_db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'db',
+    #     'PORT': '3306',
+    # },
 }
 
 
