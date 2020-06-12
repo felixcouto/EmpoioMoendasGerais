11a4a03ba780dd3aa4f590d899cd91740b8f0d8a
Felipe <felipe@MacBook-Pro.local>
2020-06-12 20:31:46 -0300

    static_files

--- emporio/urls.py
+++ emporio/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -25,3 +28,6 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', register_page),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
