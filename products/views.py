5042f3638d1d37c17ab6d0cdb4ffcf9dcc83450b
Felipe <felipe@MacBook-Pro.local>
2020-06-15 14:38:21 -0300

    Create CBF and FBV Product view

--- products/views.py
+++ products/views.py
@@ -8,6 +8,11 @@ from .models import Product
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
+    template_name = "products/list.html"
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
 
 #Function Based View
 def product_list_view(request):
