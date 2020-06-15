ae3b1620a48fdde89b27b3d25591d7f647f13307
Felipe <felipe@MacBook-Pro.local>
2020-06-15 11:40:38 -0300

    Alterando models in products

--- products/views.py
+++ products/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from django.db import models
 
-# Create your models here.
-class Product(models.Model): #product_category
-    title       = models.CharField(max_length=120)
-    description = models.TextField()
-    price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+# Create your views here.
