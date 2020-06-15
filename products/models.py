d6f1fa2b747e77695cf74c74a43d91216eb56278
Felipe <felipe@MacBook-Pro.local>
2020-06-15 12:12:52 -0300

    Alterando models in products

--- products/models.py
+++ products/models.py
@@ -5,3 +5,7 @@ class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+
+    history       = models.CharField(max_length=200)
+    harmonizacao  = models.CharField(max_length=120)
+    premios       = models.CharField(max_length=120)
