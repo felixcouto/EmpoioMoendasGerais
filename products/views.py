fc0aea3c09cde37b3a51dcf13d3bfc33fdb66595
Felipe <felipe@MacBook-Pro.local>
2020-06-15 22:12:28 -0300

    Create Custom Product Manager Class in products/views.py

--- products/views.py
+++ products/views.py
@@ -26,7 +26,7 @@ def product_list_view(request):
 #Class Based View
 class ProductDetailView(DetailView):
     #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
     
     def get_context_data(self, *args, **kwargs):
@@ -34,19 +34,31 @@ class ProductDetailView(DetailView):
         print(context)
         return context
 
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
+
+
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
     #instance = Product.objects.get(pk = pk) #get the object id
     #instance = get_object_or_404(Product, pk = pk)
 
-    qs = Product.objects.filter(id = pk)
-    if qs.count() == 1:
-        instance = qs.first()
-    else:
+    # qs = Product.objects.filter(id = pk)
+    # if qs.count() == 1:
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Esse produto não existe!")
+
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
         raise Http404("Esse produto não existe!")
 
-    
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     
     context = {
         'object': instance
