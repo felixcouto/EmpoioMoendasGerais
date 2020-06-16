f79f1972899828db6b450f3260827b8c551114d5
Felipe <felipe@MacBook-Pro.local>
2020-06-16 10:28:07 -0300

    Create src/products/utils.py and update src/products/models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator
 
 # Create your models here.
 
@@ -30,7 +32,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
-    slug        = models.SlugField(unique = True)
+    slug        = models.SlugField(blank = True, unique = True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image       = models.ImageField(upload_to = 'products/', null = True, blank = True)
@@ -50,3 +52,8 @@ class Product(models.Model): #product_category
     def __unicode__(self):
         return self.title
 
+    def product_pre_save_receiver(sender, instance, *args, **kwargs):
+        if not instance.slug:
+            instance.slug = unique_slug_generator(instance)
+
+    pre_save.connect(product_pre_save_receiver, sender = Product)
